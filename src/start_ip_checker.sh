8e363b745319d3d2e34e261184f0e015e8b9e03c
Aivin V. Solatorio <avsolatorio@gmail.com>
2017-01-05 01:59:46 +0800

    Redirect stderr to stdout.

--- src/start_ip_checker.sh
+++ src/start_ip_checker.sh
@@ -2,5 +2,5 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 cd ~/ip-change-notification/src
 
-nohup stdbuf -oL python public_ip_checker.py > nohup.out &
+nohup stdbuf -oL python public_ip_checker.py > nohup.out 2>&1 &
 
