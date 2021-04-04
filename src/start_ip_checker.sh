def60959c2012731e31b32323fd3f968bb893b6a
Aivin V. Solatorio <avsolatorio@gmail.com>
2021-04-03 20:16:55 -0400

    Update checker script.

--- src/start_ip_checker.sh
+++ src/start_ip_checker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PATH=/home/avsolatorio/ml-ai/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+# PATH=/home/avsolatorio/ml-ai/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+PATH=/mnt/Datastore/ubuntu-2016/home/avsolatorio/ml-ai/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 cd ~/ip-change-notification/src
 
 nohup stdbuf -oL python public_ip_checker.py > nohup.out 2>&1 &
